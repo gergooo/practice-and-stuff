52e1ef47c0c265e2f24ad4733097be9232ddecc5
gergooo <39014407+gergooo@users.noreply.github.com>
2020-02-08 12:38:44 +0100

    NumberOfIslands: refactored solution

--- src/number-of-islands.js
+++ src/number-of-islands.js
@@ -1,23 +1,52 @@
 'use strict';
 
-function getNumberOfIslands(islandIdArray) {
+function getNumberOfIslands(islandArray) {
   let numberOfIslands = 0;
 
-  const maxCol = islandIdArray[0].length;
-  for (let row = 0; row < islandIdArray.length; row++) {
-    for (let col = 0; col < islandIdArray[row].length; col++) {
-      if (islandIdArray[row][col] != 0) {
-        if (!((col > 0 && islandIdArray[row][col - 1] != 0) ||
-        (col > 0 && row > 0 && islandIdArray[row - 1][col - 1] != 0) ||
-        (row > 0 && islandIdArray[row - 1][col] != 0) ||
-        (col < (maxCol - 1) && row > 0 && islandIdArray[row - 1][col + 1] != 0))) {
-          numberOfIslands++;
-        }
+  for (let row = 0; row < islandArray.length; row++) {
+    for (let col = 0; col < islandArray[row].length; col++) {
+      if (isItANewIsland(islandArray, row, col)) {
+        numberOfIslands++;
       }
     }
   }
 
   return numberOfIslands;
 };
+
+function isItANewIsland(islandIdArray, row, col) {
+  return isLand(islandIdArray, row, col) &&
+        !hasVisitedNeighbours(islandIdArray, col, row);
+}
+
+function isLand(islandIdArray, row, col) {
+  return islandIdArray[row][col] != 0;
+}
+
+function hasVisitedNeighbours(islandIdArray, col, row) {
+  return (hasLeftNeighbour(col, islandIdArray, row) ||
+        hasUpperLeftNeighbour(col, row, islandIdArray) ||
+        hasUpperNeighbour(row, islandIdArray, col) ||
+        hasUpperRightNeighbour(col, row, islandIdArray));
+}
+
+function hasLeftNeighbour(col, islandIdArray, row) {
+  return (col > 0 && isLand(islandIdArray, row, col - 1));
+}
+
+function hasUpperLeftNeighbour(col, row, islandIdArray) {
+  return (col > 0 && row > 0 && isLand(islandIdArray, row - 1, col - 1));
+}
+
+function hasUpperNeighbour(row, islandIdArray, col) {
+  return (row > 0 && isLand(islandIdArray, row - 1, col));
+}
+
+function hasUpperRightNeighbour(col, row, islandIdArray) {
+  const numberOfColumns = islandIdArray[row].length;
+
+  return (col < (numberOfColumns - 1) && row > 0 &&
+     isLand(islandIdArray, row - 1, col + 1));
+}
 
 module.exports = {getNumberOfIslands};
